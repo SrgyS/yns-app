20746811fb593cbe69a4302dd5c35f972a1712e9
SrgyS <stepanov_sr@mail.ru>
2025-06-11 11:07:42 +0300

    fix: correct path for shadcn/ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/shared/ui/button";
+import { Input } from "@/shared/ui/input";
 
 export default function Home() {
   return (
-    <div >
-     <Button>Click me</Button>
+    <div>
+      <Button>Click me</Button>
+      <Input placeholder="Enter your name" />
     </div>
   );
 }
