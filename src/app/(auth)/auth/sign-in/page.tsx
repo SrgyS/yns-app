08881c1cdd7a72c2b9afcd6a6fe1e4597112bd99
SrgyS <stepanov_sr@mail.ru>
2025-06-18 17:18:33 +0300

    fix: add Suspense for sign-in-form

--- src/app/(auth)/auth/sign-in/page.tsx
+++ src/app/(auth)/auth/sign-in/page.tsx
@@ -1,6 +1,16 @@
 import { SignInForm } from '@/features/auth/sign-in-form.server'
 import { Card, CardContent, CardHeader } from '@/shared/ui/card'
 import Link from 'next/link'
+import { Suspense } from 'react'
+
+// Компонент-обертка для клиентских компонентов с useSearchParams
+function SignInFormWrapper() {
+  return (
+    <Suspense fallback={<div>Загрузка ...</div>}>
+      <SignInForm />
+    </Suspense>
+  )
+}
 
 export default function AuthenticationPage() {
   return (
@@ -11,7 +21,7 @@ export default function AuthenticationPage() {
             <h1 className="text-2xl font-semibold tracking-tight">Войти в аккаунт</h1>
           </CardHeader>
           <CardContent className="grid gap-4">
-            <SignInForm />
+            <SignInFormWrapper />
             <p className="px-0 text-center text-sm text-muted-foreground">
               Нажимая продолжить вы соглашаетесь с{' '}
               <Link href="/terms" className="underline underline-offset-4 hover:text-primary">
