4810e007854bd3c2053f44c2126dedb014b2a57d
SrgyS <stepanov_sr@mail.ru>
2025-06-18 17:33:44 +0300

    refactor: add sign-in-form skeleton

--- src/app/(auth)/auth/sign-in/page.tsx
+++ src/app/(auth)/auth/sign-in/page.tsx
@@ -1,12 +1,46 @@
 import { SignInForm } from '@/features/auth/sign-in-form.server'
 import { Card, CardContent, CardHeader } from '@/shared/ui/card'
+import { Skeleton } from '@/shared/ui/skeleton'
 import Link from 'next/link'
 import { Suspense } from 'react'
+
+// Skeleton для формы входа
+function SignInFormSkeleton() {
+  return (
+    <div className="grid gap-6">
+      {/* Email форма skeleton */}
+      <div className="grid gap-2">
+        {/* Email input skeleton */}
+        <div className="space-y-2">
+          <Skeleton className="h-10 w-full" /> {/* Input */}
+        </div>
+        {/* Submit button skeleton */}
+        <Skeleton className="h-10 w-full" />
+      </div>
+
+      {/* Разделитель skeleton */}
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <Skeleton className="w-full h-px" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <Skeleton className="bg-background px-2 text-muted-foreground w-16 h-4" />
+        </div>
+      </div>
+
+      {/* OAuth кнопки skeleton - Google*/}
+      <div className="grid gap-2">
+        <Skeleton className="h-10 w-full" />
+        {/* <Skeleton className="h-10 w-full" /> */}
+      </div>
+    </div>
+  )
+}
 
 // Компонент-обертка для клиентских компонентов с useSearchParams
 function SignInFormWrapper() {
   return (
-    <Suspense fallback={<div>Загрузка ...</div>}>
+    <Suspense fallback={<SignInFormSkeleton />}>
       <SignInForm />
     </Suspense>
   )
