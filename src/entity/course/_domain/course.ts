30bee680a150a4fbbea12fc9ab8310278f818acb
SrgyS <stepanov_sr@mail.ru>
2025-07-30 10:41:51 +0300

    feat: add types for user-course-enrollment

--- src/entity/course/_domain/course.ts
+++ src/entity/course/_domain/course.ts
@@ -1,4 +1,5 @@
 import { CourseId, CourseSlug } from "@/kernel/domain/course";
+import { CompletionStatus, DayOfWeek } from '@prisma/client'
 
 export type Course ={
   id: CourseId;
@@ -22,3 +23,45 @@ export type CourseProduct =
       access: "paid";
       price: number;
     };
+
+    export type UserCourseEnrollment = {
+      id: string
+      userId: string
+      courseId: string
+      selectedWorkoutDays: DayOfWeek[]
+      startDate: Date
+      hasFeedback: boolean
+    }
+
+    export type UserDailyPlan = {
+      id: string
+      userId: string
+      enrollmentId: string
+      date: Date
+      dayNumberInCourse: number
+      isWorkoutDay: boolean
+      warmupId: string
+      mainWorkoutId?: string
+      mealPlanId: string
+      warmupProgress: CompletionStatus
+      mainWorkoutProgress: CompletionStatus
+      mealPlanProgress: CompletionStatus
+    }
+
+    export type CreateUserCourseEnrollmentParams = {
+      userId: string
+      courseId: string
+      startDate: Date
+      selectedWorkoutDays: DayOfWeek[]
+      hasFeedback?: boolean
+    }
+
+    export type GetUserDailyPlanParams = {
+      userId: string
+      date: Date
+    }
+
+    export type GetUserDailyPlanByEnrollmentParams = {
+      enrollmentId: string
+      date: Date
+    }    
